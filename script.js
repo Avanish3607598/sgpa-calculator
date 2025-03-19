72dd49025ac1f67cd1ed44334c4fb21217fdd488
Avanish Kumar Maurya <93610668+Avanish3607598@users.noreply.github.com>
2025-03-19 23:21:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,212 +1,186 @@
+let verifier = 0;
+let gradepointarr = [];
+let creditarr = [];
+let entries = []; 
+let sr = 0;
 
+let add = document.getElementById("add");
+add.addEventListener("click", () => {
+    let name = document.getElementById("subs").value;
+    let marks = parseInt(document.getElementById("omarks").value);
+    let credit = parseInt(document.getElementById("credit").value);
 
-let verifier = 0
-let gradepointarr = []
-let creditarr = []
-let sr = 1;
-let name, marks, maxmarks, grade, gradepoint, credit;
-let add = document.getElementById('add');
-add.addEventListener('click', () => {
-
-    name = document.getElementById("subs").value;
-    marks = parseInt(document.getElementById('omarks').value);
-
-    credit = parseInt(document.getElementById("credit").value)
     if (name === "") {
-        alert("Subject name can't be empty")
-
-        return
+        alert("Subject name can't be empty");
+        return;
     }
 
     if (isNaN(marks) || marks === "") {
-        alert("Obtained marks can't be empty")
-        return
+        alert("Obtained marks can't be empty");
+        return;
     }
 
     if (isNaN(credit) || credit === "") {
-        alert("Credit can't be empty")
-        return
+        alert("Credit can't be empty");
+        return;
     }
 
-    if (marks < 0) {
-        alert("Obtained marks can't be negative")
-        document.getElementById('omarks').value = ''
-        return
+    if (marks < 0 || marks > 100) {
+        alert("Marks should be between 0 and 100");
+        document.getElementById("omarks").value = "";
+        return;
     }
 
-    if (marks > 100) {
-        alert("Obtained marks can't be greater than 100")
-        marks = 0
-        maxmarks = 0
-        document.getElementById('omarks').value = ''
-        return
+    if (credit < 0 || credit > 10) {
+        alert("Credit should be between 0 and 10");
+        document.getElementById("credit").value = "";
+        return;
     }
 
-    if (credit < 0) {
-        alert("credit can't be negative")
-        document.getElementById('credit').value = ''
-        return
-    }
+    let gradepoint = Math.floor(marks / 10) + 1;
+    if (gradepoint <= 4) gradepoint = 0;
 
-
-
-
-
-
-    creditarr.push(credit)
-    gradepoint = Math.floor(marks / 10) + 1;
-    if (gradepoint <= 4) gradepoint = 0
-    gradepointarr.push(gradepoint)
-
-    gradepoint
+    let grade;
     switch (gradepoint) {
-        case 10:
-            grade = 'S'
-            break
-        case 9:
-            grade = 'A'
-            break
-        case 8:
-            grade = 'B'
-            break
-        case 7:
-            grade = 'C'
-            break
-        case 6:
-            grade = 'D'
-            break
-        case 5:
-            grade = 'E'
-            break
-
-        default:
-            grade = 'F'
-
+        case 10: grade = "S"; break;
+        case 9: grade = "A"; break;
+        case 8: grade = "B"; break;
+        case 7: grade = "C"; break;
+        case 6: grade = "D"; break;
+        case 5: grade = "E"; break;
+        default: grade = "F";
     }
-    let table = document.getElementById("ptable")
-    table.classList.remove("toggler")
-    let tableBody = document.getElementById("tablebody")
-    let newRow = document.createElement("tr")
-    newRow.classList.add("storeclass")
 
-    if (gradepoint > 8)
-        newRow.classList.add("greenclass")
-    else if (gradepoint > 6 && gradepoint <= 8)
-        newRow.classList.add("yellowclass")
-    else
-        newRow.classList.add("redclass")
-    newRow.innerHTML = `
-            <td>${sr}</td>
-             <td>${name}</td>
-              <td>${marks}/100</td> 
-            
-              <td>${credit}</td>
-              <td>${gradepoint}</td>
-              <td>${grade}</td> 
-            
-            
-            `;
-    tableBody.appendChild(newRow)
+  
+    let entry = { id: sr, credit, gradepoint };
+    entries.push(entry);
     sr++;
-    alert(`${name} added succesfully`)
-    verifier = 1
+
+    let table = document.getElementById("ptable");
+    table.classList.remove("toggler");
+    let tableBody = document.getElementById("tablebody");
+    let newRow = document.createElement("tr");
+    newRow.classList.add("storeclass");
+    newRow.setAttribute("data-id", entry.id); 
+
+    let button2 = document.createElement("button");
+    button2.innerHTML = `Delete`;
+    button2.classList.add("edit_button");
+
+    button2.addEventListener("click", () => {
+        deleteEntry(entry.id);
+        newRow.remove();
+    });
+
+    newRow.innerHTML = `
+        <td>${name}</td>
+        <td>${marks}/100</td> 
+        <td>${credit}</td>
+        <td>${gradepoint}</td>
+        <td>${grade}</td> 
+        <td></td>  
+    `;
+    newRow.lastElementChild.appendChild(button2);
+    tableBody.appendChild(newRow);
+
+    recalculateSGPA();
+
+    document.getElementById("subs").value = "";
+    document.getElementById("credit").value = "";
+    document.getElementById("omarks").value = "";
+
+    document.getElementById("subs").classList.remove("input_accept");
+    document.getElementById("omarks").classList.remove("input_accept");
+    document.getElementById("credit").classList.remove("input_accept");
+
+    add.innerHTML = `Add Another Subject`;
+});
 
 
-
-    document.getElementById('subs').value = ''
-    document.getElementById('credit').value = ''
-    document.getElementById('omarks').value = ''
-
-    document.getElementById("subs").classList.remove("input_accept")
-    document.getElementById("omarks").classList.remove("input_accept")
-    document.getElementById("credit").classList.remove("input_accept")
-
-    let sumofcredit = creditarr.reduce((res, el) => res + el)
-    console.log(sumofcredit)
-    let sumcreditgradearr = creditarr.map((value, i) => value * gradepointarr[i])
-    console.log(sumcreditgradearr)
-    let sumcreditgrade = sumcreditgradearr.reduce((res, el) => res + el)
-    console.log(sumcreditgrade)
-    let sgpa = sumcreditgrade / sumofcredit;
-    console.log(sgpa)
-
-    let showres = document.getElementById('sgpaid')
-    if (sgpa >= 8)
-        showres.className = "greenclass"
-    else if (sgpa > 6 && sgpa < 8)
-        showres.className = "yellowclass"
-
-    else showres.className = "redclass"
-    showres.innerHTML = `SGPA = ${sgpa.toFixed(1)}`
-    add.innerHTML=`Add Anohter Subject`
-
-})
+function deleteEntry(id) {
+    entries = entries.filter(entry => entry.id !== id); 
+    recalculateSGPA(); 
+}
 
 
+function recalculateSGPA() {
+    if (entries.length === 0) {
+        document.getElementById("sgpaid").innerHTML = `SGPA: 0`;
+        document.getElementById("perid").innerHTML = `Percentage: 0`;
+        return;
+    }
+
+    let sumofcredit = entries.reduce((res, el) => res + el.credit, 0);
+    let sumcreditgrade = entries.reduce((res, el) => res + el.credit * el.gradepoint, 0);
+
+    let sgpa = (sumcreditgrade / sumofcredit).toFixed(1);
+    let percentage = 10 * sgpa - 4.5;
+
+    document.getElementById("sgpaid").innerHTML = `SGPA: ${sgpa}`;
+    document.getElementById("perid").innerHTML = `Percentage: ${percentage}`;
+}
 
 
 document.getElementById("omarks").addEventListener("change", () => {
-    let m = parseInt(document.getElementById("omarks").value)
+    let m = parseInt(document.getElementById("omarks").value);
+    let inputField = document.getElementById("omarks");
+
     if (m <= 100 && m >= 0) {
-        document.getElementById("omarks").classList.add("input_accept")
-        document.getElementById("omarks").classList.remove("input_reject")
-
+        inputField.classList.add("input_accept");
+        inputField.classList.remove("input_reject");
+    } else {
+        inputField.classList.add("input_reject");
+        inputField.classList.remove("input_accept");
     }
-    else {
-        document.getElementById("omarks").classList.add("input_reject")
-        document.getElementById("omarks").classList.remove("input_accept")
-
-    }
-
-})
+});
 
 document.getElementById("credit").addEventListener("change", () => {
-    let m = parseInt(document.getElementById("credit").value)
+    let m = parseInt(document.getElementById("credit").value);
+    let inputField = document.getElementById("credit");
+
     if (m >= 0) {
-        document.getElementById("credit").classList.add("input_accept")
-        document.getElementById("credit").classList.remove("input_reject")
-
+        inputField.classList.add("input_accept");
+        inputField.classList.remove("input_reject");
+    } else {
+        inputField.classList.add("input_reject");
+        inputField.classList.remove("input_accept");
     }
-    else {
-        document.getElementById("credit").classList.add("input_reject")
-        document.getElementById("credit").classList.remove("input_accept")
-
-    }
-
-})
+});
 
 document.getElementById("subs").addEventListener("change", () => {
-    let m = parseInt(document.getElementById("subs").value)
+    let m = document.getElementById("subs").value.trim();
+    let inputField = document.getElementById("subs");
+
     if (m === '') {
-        document.getElementById("subs").classList.add("input_reject")
-        document.getElementById("subs").classList.remove("input_accept")
-
+        inputField.classList.add("input_reject");
+        inputField.classList.remove("input_accept");
+    } else {
+        inputField.classList.add("input_accept");
+        inputField.classList.remove("input_reject");
     }
-    else {
-        document.getElementById("subs").classList.add("input_accept")
-        document.getElementById("subs").classList.remove("input_rejectt")
-
-    }
-
-})
-
-document.getElementById("clear").addEventListener('click', () => {
-    verifier = 0
-    gradepointarr.length = 0
-    creditarr.length = 0
-    sr = 1;
-    document.querySelectorAll(".storeclass").forEach(el => el.remove())
-    document.getElementById("ptable").classList.add("toggler")
-    document.getElementById('subs').value = ''
-    document.getElementById('credit').value = ''
-    document.getElementById('omarks').value = ''
-    document.getElementById("subs").classList.remove("input_accept")
-    document.getElementById("omarks").classList.remove("input_accept")
-    document.getElementById("credit").classList.remove("input_accept")
-
-   
-
-    document.getElementById('add').innerHTML=`Add Subject`
+});
 
 
-})
+document.getElementById("clear").addEventListener("click", () => {
+    verifier = 0;
+    gradepointarr.length = 0;
+    creditarr.length = 0;
+    entries = [];
+    sr = 0;
+
+    document.querySelectorAll(".storeclass").forEach(el => el.remove());
+    document.getElementById("ptable").classList.add("toggler");
+
+    document.getElementById("subs").value = "";
+    document.getElementById("credit").value = "";
+    document.getElementById("omarks").value = "";
+
+    document.getElementById("subs").classList.remove("input_accept");
+    document.getElementById("omarks").classList.remove("input_accept");
+    document.getElementById("credit").classList.remove("input_accept");
+
+    document.getElementById("sgpaid").innerHTML = `SGPA: 0`;
+    document.getElementById("perid").innerHTML = `Percentage: 0`;
+
+    document.getElementById("add").innerHTML = `Add Subject`;
+});
